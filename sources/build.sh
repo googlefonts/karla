b08bb4e9e87da4f05b29ebec713f8098a6c77fe4
Mirko Velimirovic <bghryct@gmail.com>
2019-05-17 10:01:42 -0400

    Changed Vert Metrics to match FontBakery Suggestions

--- sources/build.sh
+++ sources/build.sh
@@ -41,59 +41,59 @@ rm -rf master_ufo/ instance_ufo/
 
 echo "Build UFOS Removed"
 
-# echo "Post processing"
+echo "Post processing"
 
-# ttfs=$(ls ../fonts/ttfs/*.ttf)
-# echo $ttfs
-# for ttf in $ttfs
-# do
-# 	gftools fix-dsig -f $ttf;
-# 	gftools fix-nonhinting $ttf "$ttf.fix";
-# 	mv "$ttf.fix" $ttf;
-# done
-# rm ../fonts/ttfs/*backup*.ttf
+ttfs=$(ls ../fonts/ttfs/*.ttf)
+echo $ttfs
+for ttf in $ttfs
+do
+	gftools fix-dsig -f $ttf;
+	gftools fix-nonhinting $ttf "$ttf.fix";
+	mv "$ttf.fix" $ttf;
+done
+rm ../fonts/ttfs/*backup*.ttf
 
-# vfs=$(ls ../fonts/variable/*.ttf)
-# for vf in $vfs
-# do
-# 	gftools fix-dsig -f $vf;
-# 	gftools fix-nonhinting $vf "$vf.fix";
-# 	mv "$vf.fix" $vf;
-# 	ttx -f -x "MVAR" $vf; # Drop MVAR. Table has issue in DW
-# 	rtrip=$(basename -s .ttf $vf)
-# 	new_file=../fonts/variable/$rtrip.ttx;
-# 	rm $vf;
-# 	ttx $new_file
-# 	rm ../fonts/variable/*.ttx
-# done
-# rm ../fonts/variable/*backup*.ttf
+vfs=$(ls ../fonts/variable/*.ttf)
+for vf in $vfs
+do
+	gftools fix-dsig -f $vf;
+	gftools fix-nonhinting $vf "$vf.fix";
+	mv "$vf.fix" $vf;
+	ttx -f -x "MVAR" $vf; # Drop MVAR. Table has issue in DW
+	rtrip=$(basename -s .ttf $vf)
+	new_file=../fonts/variable/$rtrip.ttx;
+	rm $vf;
+	ttx $new_file
+	rm ../fonts/variable/*.ttx
+done
+rm ../fonts/variable/*backup*.ttf
 
-# gftools fix-vf-meta $vfs;
-# for vf in $vfs
-# do
-# 	mv "$vf.fix" $vf;
-# done
+gftools fix-vf-meta $vfs;
+for vf in $vfs
+do
+	mv "$vf.fix" $vf;
+done
 
-
+echo "Post processing complete"
 
 # cd ..
 
 # # ============================================================================
 # # Autohinting ================================================================
 
-# statics=$(ls fonts/ttfs/*.ttf)
-# echo hello
-# for file in $statics; do 
-#     echo "fix DSIG in " ${file}
-#     gftools fix-dsig --autofix ${file}
+statics=$(ls fonts/ttfs/*.ttf)
+echo hello
+for file in $statics; do 
+    echo "fix DSIG in " ${file}
+    gftools fix-dsig --autofix ${file}
 
-#     echo "TTFautohint " ${file}
-#     # autohint with detailed info
-#     hintedFile=${file/".ttf"/"-hinted.ttf"}
-#     ttfautohint -I ${file} ${hintedFile} 
-#     cp ${hintedFile} ${file}
-#     rm -rf ${hintedFile}
-# done
+    echo "TTFautohint " ${file}
+    # autohint with detailed info
+    hintedFile=${file/".ttf"/"-hinted.ttf"}
+    ttfautohint -I ${file} ${hintedFile} 
+    cp ${hintedFile} ${file}
+    rm -rf ${hintedFile}
+done
 
 
 # ============================================================================
