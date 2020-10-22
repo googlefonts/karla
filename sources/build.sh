0c03239ecf3f19273204460cfdb8c9d4f1cd8810
Viviana Monsalve <viviana.monsalve.a@gmail.com>
2020-10-22 13:02:55 -0500

    build includes italics

--- sources/build.sh
+++ sources/build.sh
@@ -3,53 +3,62 @@ set -e
 
 mkdir -p ../fonts ../fonts/ttf ../fonts/ttf/static ../fonts/otf #../fonts/woff2
 
+function postprocess_vf {
+    gftools fix-nonhinting $1 $1.fix
+    mv $1.fix $1
+    gftools fix-dsig -f $1
+    gftools fix-unwanted-tables $1 -t MVAR
+}
+
 echo "GENERATING VFs"
 VF_FILE=../fonts/ttf/Karla\[wght]\.ttf
 fontmake -g Karla-Roman.glyphs -o variable --output-path $VF_FILE
 
+VF_FILEit=../fonts/ttf/Karla-Italic\[wght]\.ttf
+fontmake -g Karla-Italic.glyphs -o variable --output-path $VF_FILEit
+
 rm -rf master_ufo/ instance_ufo/
 
 echo "POST PROCESSING VFs"
-gftools fix-nonhinting $VF_FILE $VF_FILE.fix
-mv $VF_FILE.fix $VF_FILE
+
+postprocess_vf $VF_FILE
+postprocess_vf $VF_FILEit
+
+python3 gen_stat.py $VF_FILE
 
 rm ../fonts/ttf/*gasp.ttf
 
-gftools fix-dsig -f $VF_FILE
-gftools fix-unwanted-tables $VF_FILE -t MVAR
-# python3 gen_stat.py $VF_FILE
 
+echo "GENERATING TTFs"
+fontmake -g Karla-Roman.glyphs -i -o ttf --output-dir ../fonts/ttf/static
 
-# echo "GENERATING TTFs"
-# fontmake -g Karla-Roman.glyphs -i -o ttf --output-dir ../fonts/ttf/static
-
-# echo "POST PROCESSING TTFs"
-# ttfs=$(ls ../fonts/ttf/static/*.ttf)
-# for ttf in $ttfs
-# do
-# 	ttfautohint $ttf "$ttf.fix";
-# 	mv "$ttf.fix" $ttf;
+echo "POST PROCESSING TTFs"
+ttfs=$(ls ../fonts/ttf/static/*.ttf)
+for ttf in $ttfs
+do
+	ttfautohint $ttf "$ttf.fix";
+	mv "$ttf.fix" $ttf;
 	
-# 	gftools fix-dsig -f $ttf;
+	gftools fix-dsig -f $ttf;
     
-# 	gftools fix-hinting $ttf;
-#     mv "$ttf.fix" $ttf;
+	gftools fix-hinting $ttf;
+    mv "$ttf.fix" $ttf;
 	
-# 	#compressing for woff2
-# 	fonttools ttLib.woff2 compress $ttf
+	#compressing for woff2
+	#fonttools ttLib.woff2 compress $ttf
     
-# done
+done
 
-# echo "GENERATING OTFs"
-# fontmake -g Karla-Roman.glyphs -i -o otf --output-dir ../fonts/otf/
+echo "GENERATING OTFs"
+fontmake -g Karla-Roman.glyphs -i -o otf --output-dir ../fonts/otf/
 
-# echo "POST PROCESSING OTFs"
-# otfs=$(ls ../fonts/otf/*.otf)
-# for otf in $otfs
-# do
-#     gftools fix-dsig -f $otf;
-#     psautohint $otf;
-# done
+echo "POST PROCESSING OTFs"
+otfs=$(ls ../fonts/otf/*.otf)
+for otf in $otfs
+do
+    gftools fix-dsig -f $otf;
+    psautohint $otf;
+done
 
 
 # echo "WOFF2 for static and vf"
